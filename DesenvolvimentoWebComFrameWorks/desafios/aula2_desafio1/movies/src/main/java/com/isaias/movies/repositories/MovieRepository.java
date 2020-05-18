8513248b32005728f5ac430609a9ed7c2802291a
IsaiasRider <isaiasdelima1@gmail.com>
2020-05-17 22:56:28 -0300

    Really First Commit

---
+++ DesenvolvimentoWebComFrameWorks/desafios/aula2_desafio1/movies/src/main/java/com/isaias/movies/repositories/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.isaias.movies.repositories;
+
+import com.isaias.movies.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie , Long> {
+
+
+}
