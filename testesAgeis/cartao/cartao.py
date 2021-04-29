91f5b7baa5a2e1b4805e9e385fc24b76aa0a138e
Isaias <isaiasdelima1@gmail.com>
2021-04-28 23:26:59 -0300

    refacting the function

--- testesAgeis/cartao/cartao.py
+++ testesAgeis/cartao/cartao.py
@@ -1,5 +1,8 @@
-
-
-
-def debito(zona,prazo):
-    return 6
+def debito(zona,plano):
+    if zona == "A" and plano == "unico":
+      valor_plano_escolhido = 6
+    elif zona == "A" and plano == "dia":
+      valor_plano_escolhido = 10
+    elif zona == "B" and plano == "unico":
+      valor_plano_escolhido = 7
+    return valor_plano_escolhido
--- testesAgeis/cartao/test_cartao.py
+++ testesAgeis/cartao/test_cartao.py
@@ -1,12 +1,16 @@
+#encoding:utf-8--##
 import pytest
 import cartao
+import sys
+
+
 def test_zona_a_unica_vez_debito_6():
     expected = 6
     result = cartao.debito("A","unico")
     assert expected == result
 
-def test_zona_b_unica_vez_debito_8():
-    expected = 8
+def test_zona_b_unica_vez_debito_7():
+    expected = 7
     result = cartao.debito("B","unico")
     assert expected == result
 
