4898af04b375511f42c706cd15e8b7b508f11ea6
Isaias <isaiasdelima1@gmail.com>
2021-05-01 21:13:24 -0300

    cover all debit plan

--- testesAgeis/cartao/cartao.py
+++ testesAgeis/cartao/cartao.py
@@ -3,6 +3,16 @@ def debito(zona,plano):
       valor_plano_escolhido = 6
     elif zona == "A" and plano == "dia":
       valor_plano_escolhido = 10
+    elif zona == "A" and plano == "semana":
+        valor_plano_escolhido = 30
+    elif zona == "A" and plano == "mes":
+        valor_plano_escolhido = 130
     elif zona == "B" and plano == "unico":
       valor_plano_escolhido = 7
+    elif zona == "B" and plano == "dia":
+        valor_plano_escolhido = 12
+    elif zona == "B" and plano == "semana":
+        valor_plano_escolhido = 45
+    elif zona == "B" and plano == "mes":
+        valor_plano_escolhido = 170
     return valor_plano_escolhido
--- testesAgeis/cartao/test_cartao.py
+++ testesAgeis/cartao/test_cartao.py
@@ -9,12 +9,37 @@ def test_zona_a_unica_vez_debito_6():
     result = cartao.debito("A","unico")
     assert expected == result
 
+def test_zona_a_dia_debito():
+    expected = 10
+    result = cartao.debito("A","dia")
+    assert expected == result
+
+def test_zona_a_semana_debito():
+    expected = 30
+    result = cartao.debito("A","semana")
+    assert expected == result
+
+def test_zona_a_mes_debito():
+    expected = 130
+    result = cartao.debito("A","mes")
+    assert expected == result
+
 def test_zona_b_unica_vez_debito_7():
     expected = 7
     result = cartao.debito("B","unico")
     assert expected == result
 
-def test_zona_a_dia_debito():
-    expected = 10
-    result = cartao.debito("A","dia")
+def test_zona_b_dia_debito():
+    expected = 12
+    result = cartao.debito("B","dia")
+    assert expected == result
+
+def test_zona_b_semana_debito():
+    expected = 45
+    result = cartao.debito("B","semana")
+    assert expected == result
+
+def test_zona_b_mes_debito():
+    expected = 170
+    result = cartao.debito("B","mes")
     assert expected == result
